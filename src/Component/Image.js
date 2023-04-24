569c58da19fbe896be6ead1136e4fe8792c0e5e9
unknown <sutharmohit409@gmail.com>
2023-04-24 17:57:39 +0530

    new onw

--- src/Component/Image.js
+++ src/Component/Image.js
@@ -1,4 +1,6 @@
+import { colors } from '@mui/material'
 import React from 'react'
+import "../App.css"
 
 export default function Image() {
   return (
@@ -7,6 +9,7 @@ export default function Image() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Google_Tasks_2021.svg/1200px-Google_Tasks_2021.svg.png"
           width={100}
         />
+        <h1 className='textDeco'>TOdO APPlicaTiON</h1>
     </div>
   )
 }
